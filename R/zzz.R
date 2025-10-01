7606ccba9610b761905f3a9802e1955329b862fe
Christoph Sax <christoph.sax@gmail.com>
2025-10-01 18:29:18 +0200

    upd

--- R/zzz.R
+++ R/zzz.R
@@ -3,20 +3,24 @@ register_rtf_blocks <- function() {
   register_blocks(
     c(
       "new_rtf_block",
+      "new_rtf_to_df_block",
       "new_topline_block",
       "new_card_block"
     ),
     name = c(
       "Generic RTF file parser",
+      "RTF to dataframe parser",
       "RTF ARD parser for example data",
       "RTF CARD parser for example data"
     ),
     description = c(
       "Read and parse RTF file to ARD data.frame",
+      "Read and parse RTF file to data.frame",
       "Parse specific tables to ARD data.frame",
       "Parse specific tables to CARD"
     ),
     category = c(
+      "data",
       "data",
       "data",
       "data"
